26cd06a1ec0fc25d50bd9ac0a0d8e14281269f30
ferdie bergado <ferdiebergado@gmail.com>
2025-08-08 13:36:04 +0800

    chore: Install and configure shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 /// <reference types="vitest/config" />
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
     test: {
         projects: [
             {
