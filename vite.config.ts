14df1037ac38c80ef42c487cdf7345f798d4a795
ferdie bergado <ferdiebergado@gmail.com>
2025-08-08 10:53:35 +0800

    chore: Install and configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest/config" />
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    test: {
+        projects: [
+            {
+                extends: true,
+                test: {
+                    name: 'unit',
+                    environment: 'node',
+                    include: ['./tests/unit/**/*.{test,spec}.ts'],
+                },
+            },
+            {
+                extends: true,
+                test: {
+                    name: 'component',
+                    browser: {
+                        enabled: true,
+                        headless: true,
+                        provider: 'playwright',
+                        instances: [{ browser: 'chromium' }],
+                    },
+                    include: ['./tests/component/**/*.{test,spec}.tsx'],
+                    setupFiles: './vitest-setup-client.ts',
+                },
+            },
+        ],
+    },
 })
