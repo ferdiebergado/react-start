fc73b160e2ab4c5498ee643437e73c92eb13331b
ferdie bergado <ferdiebergado@gmail.com>
2025-08-08 14:46:46 +0800

    refactor: Increase top and bottom margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="text-muted-foreground text-center text-xs">
+        <footer className="text-muted-foreground my-6 text-center text-xs">
             &copy; 2025 by ferdie bergado
         </footer>
     )
