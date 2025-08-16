315105c7d90d0ff06be6fd59b70061fc37a68e16
ferdie bergado <ferdiebergado@gmail.com>
2025-08-16 15:08:12 +0800

    refactor: Change top border color to cyan

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,7 +3,7 @@ import { type FC } from 'react'
 const Spinner: FC = () => {
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center">
-            <div className="h-12 w-12 animate-spin rounded-full border-4 border-t-4 border-gray-200 border-t-blue-500"></div>
+            <div className="h-12 w-12 animate-spin rounded-full border-4 border-t-4 border-gray-200 border-t-cyan-500"></div>
         </div>
     )
 }
