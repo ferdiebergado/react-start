5f8ab093b3e239cb4bc3c89160bdae01976997c8
ferdie bergado <ferdiebergado@gmail.com>
2025-08-25 15:46:57 +0800

    refactor: Subpaths should set isActive to true

--- src/components/NavigationLink.tsx
+++ src/components/NavigationLink.tsx
@@ -1,16 +1,20 @@
 import { NavigationMenuLink } from '@/components/ui/navigation-menu'
 import { type FC } from 'react'
-import { NavLink, useLocation, type NavLinkProps } from 'react-router'
+import { NavLink, useMatch, type NavLinkProps } from 'react-router'
 
 const NavigationLink: FC<
     NavLinkProps & React.RefAttributes<HTMLAnchorElement>
 > = ({ to, ...props }) => {
-    const { pathname } = useLocation()
-    const isActive = pathname === to
+    const toPath = typeof to === 'string' ? to : (to.pathname ?? '/')
+    const match = useMatch(toPath.endsWith('/*') ? toPath : `${toPath}/*`)
 
     return (
-        <NavigationMenuLink asChild active={isActive}>
-            <NavLink to={to} {...props} />
+        <NavigationMenuLink asChild active={!!match}>
+            <NavLink
+                to={to}
+                {...props}
+                className="data-[active]:decoration-accent-foreground no-underline data-[active]:underline data-[active]:decoration-2 data-[active]:underline-offset-8"
+            />
         </NavigationMenuLink>
     )
 }
