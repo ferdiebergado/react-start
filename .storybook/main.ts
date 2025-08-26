ece07a191f52b4434b71d3e8781f0afaaee0fd1c
ferdie bergado <ferdiebergado@gmail.com>
2025-08-26 16:49:30 +0800

    chore: Setup msw-storybook-addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,12 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": ["@storybook/addon-themes"],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
-};
-export default config;
+    stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: ['@storybook/addon-themes'],
+    framework: {
+        name: '@storybook/react-vite',
+        options: {},
+    },
+    staticDirs: ['../public'],
+}
+export default config
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,9 @@
 import { withThemeByClassName } from '@storybook/addon-themes'
 import type { Preview } from '@storybook/react-vite'
+import { initialize, mswLoader } from 'msw-storybook-addon'
 import '../src/index.css'
+
+initialize()
 
 export const decorators = [
     withThemeByClassName({
@@ -21,6 +24,7 @@ const preview: Preview = {
             },
         },
     },
+    loaders: [mswLoader],
 }
 
 export default preview
