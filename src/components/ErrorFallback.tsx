7f8f4f133c4934b4efc03aa22a2fbb1705532ed9
ferdie bergado <ferdiebergado@gmail.com>
2025-08-26 11:44:52 +0800

    refactor: Show error message

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -1,20 +1,29 @@
+import ErrorWrapper from '@/components/ErrorWrapper'
 import { Button } from '@/components/ui/button'
 import type { FC } from 'react'
 import type { FallbackProps } from 'react-error-boundary'
-import ErrorWrapper from './ErrorWrapper'
 
-const ErrorFallback: FC<FallbackProps> = ({ resetErrorBoundary }) => (
-    <ErrorWrapper>
-        <h1 className="text-destructive text-6xl">Something went wrong.</h1>
-        <Button
-            className="bg-foreground text-background m-4 p-4"
-            onClick={() => {
-                resetErrorBoundary()
-            }}
-        >
-            Try again
-        </Button>
-    </ErrorWrapper>
-)
+const ErrorFallback: FC<FallbackProps> = ({ error, resetErrorBoundary }) => {
+    const message =
+        error instanceof Error
+            ? error.message
+            : typeof error === 'string'
+              ? error
+              : 'Unknown error'
+
+    const handleClick = () => {
+        resetErrorBoundary()
+    }
+
+    return (
+        <ErrorWrapper>
+            <h1 className="text-muted-foreground text-6xl">
+                Something went wrong.
+            </h1>
+            <pre className="text-destructive">{message}</pre>
+            <Button onClick={handleClick}>Try again</Button>
+        </ErrorWrapper>
+    )
+}
 
 export default ErrorFallback
