8e7c0ac6777bfd70301222c33e67c64d61ac0a72
ferdie bergado <ferdiebergado@gmail.com>
2025-08-26 16:15:50 +0800

    chore: Create NavigationLink story

---
+++ src/components/NavigationLink.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import NavigationLink from '@/components/NavigationLink'
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuList,
+} from '@/components/ui/navigation-menu'
+import { MemoryRouter } from 'react-router'
+
+const meta = {
+    component: NavigationLink,
+    parameters: {
+        layout: 'centered',
+    },
+    decorators: [
+        (Story) => (
+            <MemoryRouter>
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <Story />
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </MemoryRouter>
+        ),
+    ],
+} satisfies Meta<typeof NavigationLink>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: { to: '/', children: 'Home' },
+}
