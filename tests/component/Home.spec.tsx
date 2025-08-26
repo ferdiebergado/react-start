f3b622adc68644ee460d9967856e3801e68688a9
ferdie bergado <ferdiebergado@gmail.com>
2025-08-26 13:09:03 +0800

    test: Remove fetch stub

--- tests/component/Home.spec.tsx
+++ tests/component/Home.spec.tsx
@@ -1,34 +1,11 @@
-import type { Quote } from '@/features/quote'
 import Home from '@/Home'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { MemoryRouter, Route, Routes } from 'react-router'
-import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { render } from 'vitest-browser-react'
 
 describe('Home', () => {
-    const mockQuote: Quote = {
-        id: 1,
-        quote: 'Stay hungry, stay foolish.',
-        author: 'Steve Jobs',
-    }
-
-    beforeAll(() => {
-        vi.stubGlobal(
-            'fetch',
-            vi.fn(() =>
-                Promise.resolve({
-                    ok: true,
-                    json: () => Promise.resolve(mockQuote),
-                })
-            )
-        )
-    })
-
-    afterAll(() => {
-        vi.unstubAllGlobals()
-    })
-
-    it.concurrent('renders quote and author', async () => {
+    it('renders quote and author', async () => {
         const queryClient = new QueryClient()
 
         const { getByText } = render(
