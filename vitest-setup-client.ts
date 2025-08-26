f226bd26f5692da703310a9493770336b13cc98c
ferdie bergado <ferdiebergado@gmail.com>
2025-08-26 13:08:47 +0800

    chore: Setup msw for mocking requests

--- vitest-setup-client.ts
+++ vitest-setup-client.ts
@@ -1,1 +1,24 @@
 /// <reference types="@vitest/browser/providers/playwright" />
+import { http, HttpResponse } from 'msw'
+import { setupWorker } from 'msw/browser'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+
+const handlers = [
+    http.get('https://dummyjson.com/quotes/random', () => {
+        return HttpResponse.json({
+            id: 1,
+            quote: 'Stay hungry, stay foolish.',
+            author: 'Steve Jobs',
+        })
+    }),
+]
+
+const worker = setupWorker(...handlers)
+
+beforeAll(async () => await worker.start())
+afterAll(() => {
+    worker.stop()
+})
+afterEach(() => {
+    worker.resetHandlers()
+})
