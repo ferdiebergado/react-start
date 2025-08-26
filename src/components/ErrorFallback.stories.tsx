401f1f809a138a052e882526bb0634e8009efbf8
ferdie bergado <ferdiebergado@gmail.com>
2025-08-26 15:42:46 +0800

    chore: Add more stories

--- src/components/ErrorFallback.stories.tsx
+++ src/components/ErrorFallback.stories.tsx
@@ -14,3 +14,14 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
     args: { error: null, resetErrorBoundary: fn() },
 }
+
+export const WithError: Story = {
+    args: {
+        error: new Error('Internal Server Error'),
+        resetErrorBoundary: fn(),
+    },
+}
+
+export const WithStringError: Story = {
+    args: { error: 'Network request failed.', resetErrorBoundary: fn() },
+}
