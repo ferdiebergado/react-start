968ffb8baa85bcdb7c91f65fc2e94deed16645c5
ferdie bergado <ferdiebergado@gmail.com>
2025-08-26 15:07:39 +0800

    chore: Create ErrorFallback story

---
+++ src/components/ErrorFallback.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import ErrorFallback from '@/components/ErrorFallback'
+import { fn } from 'storybook/internal/test'
+
+const meta = {
+    component: ErrorFallback,
+} satisfies Meta<typeof ErrorFallback>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: { error: null, resetErrorBoundary: fn() },
+}
