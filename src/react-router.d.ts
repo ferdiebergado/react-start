31fd36c9016a3f659dec8bbf0bc1cd82e73db459
ferdie bergado <ferdiebergado@gmail.com>
2025-09-02 21:23:45 +0800

    refactor: Augment the return type of useNavigate

---
+++ src/react-router.d.ts
@@ -0,0 +1,8 @@
+import type { NavigateOptions, To } from 'react-router'
+
+declare module 'react-router' {
+    interface NavigateFunction {
+        (to: To, options?: NavigateOptions): void
+        (delta: number): void
+    }
+}
