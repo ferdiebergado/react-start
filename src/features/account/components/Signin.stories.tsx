dbbf0ca12d83ecaca81b08452bd308be2553215a
ferdie bergado <ferdiebergado@gmail.com>
2025-09-05 08:22:55 +0800

    refactor: Wrap story with QueryClientProvider

--- src/features/account/components/Signin.stories.tsx
+++ src/features/account/components/Signin.stories.tsx
@@ -2,17 +2,22 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import AccountProvider from '@/features/account/AccountProvider';
 import Signin from '@/features/account/components/Signin';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MemoryRouter } from 'react-router';
+
+const queryClient = new QueryClient();
 
 const meta = {
   component: Signin,
   decorators: [
     (Story) => (
-      <AccountProvider>
-        <MemoryRouter>
-          <Story />
-        </MemoryRouter>
-      </AccountProvider>
+      <MemoryRouter>
+        <QueryClientProvider client={queryClient}>
+          <AccountProvider>
+            <Story />
+          </AccountProvider>
+        </QueryClientProvider>
+      </MemoryRouter>
     ),
   ],
 } satisfies Meta<typeof Signin>;
