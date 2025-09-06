7c981fb5e7aeb8b82b0f8606b24da271562ff282
ferdie bergado <ferdiebergado@gmail.com>
2025-09-06 14:33:09 +0800

    refactor: Rename RouteState to FromRouteState

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+interface FromRouteState {
+  from: string;
+}
+
+export function isFromRouteState(object: unknown): object is FromRouteState {
+  if (object === null || typeof object !== 'object') {
+    return false;
+  }
+  return 'from' in object && typeof object.from === 'string';
+}
