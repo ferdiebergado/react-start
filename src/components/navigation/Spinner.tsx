4fd1175c20149aa26dfcec0d122cb36ee90c6d5b
ferdie bergado <ferdiebergado@gmail.com>
2025-10-04 16:17:14 +0800

    refactor: Add a role attribute

--- src/components/navigation/Spinner.tsx
+++ src/components/navigation/Spinner.tsx
@@ -2,7 +2,10 @@ import { type FC } from 'react';
 
 const Spinner: FC = () => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center">
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center"
+      role="status"
+    >
       <div className="h-12 w-12 animate-spin rounded-full border-4 border-t-4 border-gray-200 border-t-cyan-500"></div>
     </div>
   );
